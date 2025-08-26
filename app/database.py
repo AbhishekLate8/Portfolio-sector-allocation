20b407cdc648e5c1b6b9db1826a924eb7ba22ecc
Abhishek Late <Abhishek.Late@Circana.com>
2025-08-26 22:53:29 +0530

    added ssl mode in database.py

--- app/database.py
+++ app/database.py
@@ -3,14 +3,17 @@ from sqlalchemy.orm import declarative_base
 from .config import settings
 from typing import AsyncGenerator
 from urllib.parse import quote_plus
+import ssl
 
-SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.database_username}:{quote_plus(settings.database_password)}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require'
+ssl_context = ssl.create_default_context(cafile=None)
+
+SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.database_username}:{quote_plus(settings.database_password)}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 
 print(SQLALCHEMY_DATABASE_URL)
 
 # Create the async engine
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False,connect_args ={"ssl":ssl_context})
 
 # Create the async session maker
 AsyncSessionLocal = async_sessionmaker(bind = engine, expire_on_commit=False, class_=AsyncSession)
